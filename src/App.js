f8508783de8355e1ca087750348c4a2af31b22e6
Frontend0123 <mohindarchandru1998@gmail.com>
2023-12-21 15:55:09 +0530

    update for redeployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import AddTransaction from './Components/AddTransaction';
+import Balance from './Components/Balance';
+import Header from './Components/Header';
+import IncomeExpense from './Components/IncomeExpense';
+import {GlobalProvider }from './Components/Context/GlobalState';
+import TransactionList from './Components/TransactionList';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header/>
+      <div className='container'>
+      <Balance/>
+      <IncomeExpense/>
+      <TransactionList/>
+      
+      <AddTransaction/>
+
+      </div>
+    
+    </GlobalProvider>
   );
 }
 
